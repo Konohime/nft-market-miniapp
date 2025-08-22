afab2bab2458a26b0455e1695bac139455b0ede0
Konohime <42278919+Konohime@users.noreply.github.com>
2025-08-22 15:53:44 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
 
-    <div className="font-sans items-center justify-items-center p-8 pb-20 gap-16 sm:p-20">
+    <div className="font-sans items-center justify-items-center">
       <main>
         <div>
         <iframe className="configIframe" src="https://dune.com/embeds/4891077/8099346"/>
