16d2712652c2a54360cd452798aaba87210f29d8
Konohime <42278919+Konohime@users.noreply.github.com>
2025-09-03 19:31:32 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Silence warnings
-  // https://github.com/WalletConnect/walletconnect-monorepo/issues/1908
-  webpack: (config) => {
-    config.externals.push("pino-pretty", "lokijs", "encoding");
-    return config;
-  },
-};
-
-export default nextConfig;
+async redirects() {
+  return [
+    {
+      source: '/.well-known/farcaster.json',
+      destination: 'https://api.farcaster.xyz/miniapps/hosted-manifest/0199108a-d959-19da-5e48-9b7a9927d062',
+      permanent: false, // triggers a 307 redirect
+    },
+  ];
+}
